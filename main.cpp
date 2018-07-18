4282ae9380daff36f313073a456dbe127e8aefad
Neod0Matrix <1054465075@qq.com>
2018-07-18 15:12:37 +0800

    Fix Linux Macro

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 /*
  * OpenCV 3.1.0 安装环境测试
- * 测试IDE：Clion 2017.3(Linux) CMake 3.9 gcc 6.3
- * 功能实现：打开摄像头(包括内部外部)捕获1920x1080图像
+ * 测试IDE：Clion 2017.3 (Linux) CMake 3.9 gcc 6.3
+ *			Visual Studio 2013 (Windows) 
+ * 功能实现：打开摄像头(优先外部)尝试捕获1920x1080图像
  * 视频保存：按1保存写入，按2停止写入，按esc退出
  * code by </MATRIX>@Neod Anderjon
  */
@@ -10,11 +11,14 @@
 using namespace std;
 using namespace cv;
 
-//设置视频保存地址
-string video_save_path = "/home/neod/Downloads/opencv_camera_test.avi";
+//设置视频保存地址，以系统区分
+#if defined(_WIN32)
+string video_save_path{ "E:\\OperationCache\\opencv_camera_test.avi" };
+#elif defined(__linux__)
+string video_save_path{ "/home/neod/Downloads/opencv_camera_test.avi" };
+#endif
 
 int main (int argc, char *argv[]) {
-    //打印标志
     printf("</MATRIX> opencv c++ test project\n");
 
     VideoCapture cap;										//创建摄像捕获
@@ -34,17 +38,19 @@ int main (int argc, char *argv[]) {
     //设置摄像头输出画面
     //cap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('D', 'I', 'V', 'X'));
     cap.set(CV_CAP_PROP_FPS, 60);
-    cap.set(CV_CAP_PROP_FRAME_WIDTH, 1920);
+	//尝试以1920x1080分频率打开，达不到的摄像头会自动调整到合适最大尺寸
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 1920);					
     cap.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
+	//static_cast<int>C++数据类型强制转换
     int video_weight = static_cast<int>(cap.get(CV_CAP_PROP_FRAME_WIDTH));
     int video_height = static_cast<int>(cap.get(CV_CAP_PROP_FRAME_HEIGHT));
     Size frame_size(video_weight, video_height);            //获得帧的宽高
     double frame_fps = cap.get(CV_CAP_PROP_FPS);            //获取fps
-    //创建视频文件
-    wrt.open(video_save_path, CV_FOURCC('D', 'I', 'V', 'X'), frame_fps, frame_size, true);
+    //创建视频文件，通用MPEG-4格式
+	wrt.open(video_save_path, CV_FOURCC('D', 'I', 'V', 'X'), frame_fps, frame_size, true);
 
     while (true) {
-        cap >> frame;										//将摄像头读取到frame流中
+		cap.read(frame);									//将摄像头读取到frame流中						
         //判断成功与否
         if (frame.empty()) {
             printf("Read frame stream error\n");
@@ -52,16 +58,21 @@ int main (int argc, char *argv[]) {
         }
         imshow("cvDemoMonitor#1", frame);					//显示图像
 
-        //按'1'开始写入视频流，按'2'结束
+        //按'1'开始写入视频流，按'2'结束，\a声音提示
         static bool startVideoflag = false;
         if (waitKey(20) == '1' && !startVideoflag) {
             startVideoflag = true;							//开始录像标志
-            printf("Start Write Video to File\n");
+            printf("\aStart write video to file\n");
+			//打印实际的视频信息
+			printf("Video weight: %d height: %d fps: %.2lf\n", 
+				video_weight, video_height, frame_fps);
+			//string特性
+			printf("Video file save to %s\n", video_save_path.c_str());
         }
         else if (waitKey(20) == '2' && startVideoflag) {
             startVideoflag = false;
             wrt.release();									//回收进程
-            printf("Finish Write Video to File\n");
+            printf("\aFinish write video to file\n");
         }
 
         if (startVideoflag)
@@ -69,10 +80,9 @@ int main (int argc, char *argv[]) {
 
         //esc退出
         if (waitKey(20) == 27) {
-            //回收进程关闭窗口
-            cap.release();
+            cap.release();					
             wrt.release();
-            cvDestroyAllWindows();
+            cvDestroyAllWindows();							//回收进程关闭窗口
 
             break;
         }
